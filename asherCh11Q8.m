4e2557db3165c51527e517ffec5665b140d29c8b
Trevor Squires <tsquire@clemson.edu>
2018-11-11 14:19:36 -0500

    Corrected Plot of question 11.8

--- asherCh11Q8.m
+++ asherCh11Q8.m
@@ -27,12 +27,24 @@ N = length(n);
 err = zeros(1,N);
 xx = 0:.001:1;
 fxx = f(xx);
+plot(xx,fxx,'k--')
+title('Cubic spline approximations of f(x)')
+xlabel('x')
+ylabel('spline(x)')
+hold on
 
+%% Computing Spline and Error
 for i = 1:N
     x = 0:1/n(i):1;
     fx = f(x);
     splinex = spline(x,fx,xx);
     err(i) = max(abs(fxx-splinex));
+    plot(xx,splinex)
+    hold on
 end
 
-loglog(err)
+figure();
+loglog(n,err)
+title('Logarithmic graph of errors with respective to mesh size')
+xlabel('Mesh size')
+ylabel('Error')
