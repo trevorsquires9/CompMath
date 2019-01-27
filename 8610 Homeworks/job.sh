8296c865ae25068c4e8e172f272ad4283d487411
Trevor <tsquire@clemson.edu>
2019-01-27 18:12:17 -0500

    Managed to push from Cluster

--- 8610 Homeworks/job.sh
+++ 8610 Homeworks/job.sh
@@ -1,18 +1,16 @@
 
 #!/bin/bash
 #
-#PBS -N test_matlab
+#PBS -N xueHW1Q5
 #PBS -l select=1:ncpus=8:mem=125gb
-#PBS -l walltime=24:00:00
+#PBS -l walltime=72:00:00
 #PBS -m abe
 #PBS -M tsquire@g.clemson.edu
 #PBS -j oe
-#PBS -o errors.output
+#PBS -o errors
 
 module add matlab/2017a
 
 cd /home/tsquire/CompMath/8610\ Homeworks
 
-#taskset -c 0-$(($OMP_NUM_THREADS-1)) matlab -nodisplay -nodesktop -nosplash -r path(pwd,path())  > path_results.txt
-taskset -c 0-$(($OMP_NUM_THREADS-1)) matlab -nodisplay -nodesktop -nosplash -r xueHW1Q5 > test_results.txt
-
+taskset -c 0-$(($OMP_NUM_THREADS-1)) matlab -nodisplay -nodesktop -nosplash -r xueHW1Q5
--- 8610 Homeworks/xueHW1Q5.m
+++ 8610 Homeworks/xueHW1Q5.m
@@ -19,20 +19,24 @@ clear
 clc
 close all;
 tic
-exp = 5:11;
+
+exp = 5:10;
 n = 2.^exp;
 k = length(n);
 trials = 5000;
 counts = zeros(1,k);
+saveFreq = 100;
 
-for j = 1:k
-    counts(j) = 0;
-    for i = 1:trials
+for i = 1:trials
+    for j = 1:k
         A = randn(n(j),n(j))/sqrt(n(j));
         [~,~,growth] = pluFact(A,'pivot');
         if growth > sqrt(n(j))
             counts(j) = counts(j) + 1;
         end
+    end
+    if mod(i,saveFreq) == 0
+	save('results')
     end
 end
 time = toc;
