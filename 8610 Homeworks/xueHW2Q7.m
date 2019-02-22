d790c9b3861e5bcbb48661a55d46c3e9b45b2fc6
Trevor Squires <tsquire@clemson.edu>
2019-02-22 11:16:01 -0500

    Redid HW2Q7 861

--- 8610 Homeworks/xueHW2Q7.m
+++ 8610 Homeworks/xueHW2Q7.m
@@ -9,7 +9,7 @@
 %   None
 %
 % Notes
-%   Quite a terrible script.  But it got the job done. 
+%   Quite a terrible script.  But it got the job done.
 %
 % Author
 %   Trevor Squires
@@ -22,8 +22,7 @@ close all;
 interval = [1,2];
 n = 50;
 t = linspace(interval(1),interval(2),n);
-f = @(t) 1./t;
-
+f = @(x) 1./x;
 
 u1 = @(x) sin(x);
 u2 = @(x) exp(x);
@@ -49,9 +48,63 @@ Coeff3 = integral(@(x) 1./x .* (gamma(x) - v3Coeff1*sin(x) - v3Coeff2*(exp(x) - 
 g = @(x) Coeff1*sin(x)/v1Norm + Coeff2*(exp(x) - v2Coeff1*sin(x)) + Coeff3*(gamma(x) - v3Coeff1*sin(x) - v3Coeff2*(exp(x) + v2Coeff1*sin(x)));
 ft = f(t);
 gt = g(t);
-plot(t,ft,t,gt)
+
+subplot(1,2,1);
+plot(t,ft,'r-') 
+hold on
+plot(t,gt,'b--')
 legend ('f(t)','g(t)')
-title('Approximation of 1/x by linear combinations of sin(x),exp(x), and gamma(x)')
+title('Projection Approach')
 
 
 
+%% Alternative Approach through least squares solve
+clear
+
+syms x
+g{1} = exp(x);
+g{2} = gamma(x);
+g{3} = sin(x);
+f = @(x) 1./x;
+
+AtA = zeros(3);
+AtB = zeros(3,1);
+for i = 1:3
+    for j = 1:3
+        AtA(i,j) = vpa(int(g{i}*g{j},[1,2]));
+    end
+    AtB(i) = vpa(int(g{i}*f,[1,2]));
+end
+
+c = AtA\AtB;
+t = linspace(1,2,100);
+p = @(x) exp(x)*c(1) + gamma(x)*c(2) + sin(x)*c(3);
+
+subplot(1,2,2);
+plot(t,feval(f,t),'r-')
+hold on
+plot(t,feval(p,t),'b--')
+legend ('f(t)','g(t)')
+title('LSQ Approach')
+
+
+
+%% Not working approach
+% clear
+% clc
+% close all;
+% 
+% f = @(x) 1./x;
+% t = linspace(1,2,200);
+% 
+% ft = f(t)';
+% A = [sin(t)' exp(t)' gamma(t)'];
+% 
+% x = A\ft;
+% p = @(x) x(1)*sin(x) + x(2)*exp(x) + x(3)*gamma(x);
+% pt = p(t);
+% 
+% plot(t,ft,'r-')
+% hold on
+% plot(t,pt,'b--')
+% 
